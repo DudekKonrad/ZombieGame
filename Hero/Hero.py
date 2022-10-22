98dd41c680af7778900abcc0961811e797b1f327
kolodziejpawel98 <kolodziejpawel98@gmail.com>
2022-10-22 09:47:12 +0200

    change hero icon into triangle

--- Hero/Hero.py
+++ Hero/Hero.py
@@ -2,7 +2,7 @@ from Variables.global_variables import *
 
 
 class Hero:
-    coordinates = [0, 0]
+    coordinates = [(0, 0), (0, 0), (0, 0)]
     size = [0, 0]
     speed = 0
     color = (0, 255, 0)
@@ -14,16 +14,43 @@ class Hero:
 
     def hero_movement(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and self.coordinates[0] > 0:
-            self.coordinates[0] -= self.speed
-        if keys[pygame.K_RIGHT] and self.coordinates[0] < SCREEN_WIDTH:
-            self.coordinates[0] += self.speed
-        if keys[pygame.K_UP] and self.coordinates[1] > 0:
-            self.coordinates[1] -= self.speed
-        if keys[pygame.K_DOWN] and self.coordinates[1] < SCREEN_HEIGHT:
-            self.coordinates[1] += self.speed
+        if keys[pygame.K_LEFT] and self.coordinates[1][0] > 0:
+            self.step_left()
+        if keys[pygame.K_RIGHT] and self.coordinates[2][0] < SCREEN_WIDTH:
+            self.step_right()
+        if keys[pygame.K_UP] and self.coordinates[0][1] > 0:
+            self.step_up()
+        if keys[pygame.K_DOWN] and self.coordinates[1][1] < SCREEN_HEIGHT:
+            self.step_down()
 
     def draw_hero(self):
         self.hero_movement()
         window.fill((0, 0, 0))  # odswiezenie ekranu
-        pygame.draw.rect(window, self.color, [self.coordinates, self.size], 0)
+        pygame.draw.polygon(window, self.color, self.coordinates, 0)
+
+    def obstacle_collision(self):
+        collide = pygame.Rect.colliderect(self.coordinates, Rect(200, 0, 50, 50))
+
+    def step_left(self):
+        for i in range(3):
+            editable_vertex = list(self.coordinates[i])
+            editable_vertex[0] -= self.speed
+            self.coordinates[i] = tuple(editable_vertex)
+
+    def step_right(self):
+        for i in range(3):
+            editable_vertex = list(self.coordinates[i])
+            editable_vertex[0] += self.speed
+            self.coordinates[i] = tuple(editable_vertex)
+
+    def step_up(self):
+        for i in range(3):
+            editable_vertex = list(self.coordinates[i])
+            editable_vertex[1] -= self.speed
+            self.coordinates[i] = tuple(editable_vertex)
+
+    def step_down(self):
+        for i in range(3):
+            editable_vertex = list(self.coordinates[i])
+            editable_vertex[1] += self.speed
+            self.coordinates[i] = tuple(editable_vertex)
