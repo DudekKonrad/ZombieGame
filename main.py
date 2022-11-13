fe40061f162981ec75555b3b4f54e564a3da2ada
kolodziejpawel98 <kolodziejpawel98@gmail.com>
2022-11-13 14:24:34 +0100

    many zombies refactor

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ pygame.init()
 Hero = Hero(HERO_START_POSITION, HERO_SIZE, HERO_SPEED)
 Map = Map(window)
 Map.make_map()
-zombie = Zombie()
-# zombie2 = Zombie([100.0, 200.0])
-#radius, position, time, orientation, velocity, rotation, steering.linear, steering.angular
+
+zombies = [Zombie([500.0, 500.0]), Zombie([660.0, 600.0]), Zombie([860.0, 600.0])]
 
 running = True
 while running:
@@ -23,19 +22,9 @@ while running:
     Map.draw_map()
     for obstacle in Map.obstacles:
         Hero.check_if_collide(obstacle)
-    # zombie.kinematic_update()
-    # zombie.kinematic_arrive(Hero.get_center())
-    # zombie.wandering()
-    # zombie.collision_avoidance(Map.obstacles)
-    # zombie.update(Hero.get_center())
-    # print("zombie1 = ", zombie.wander.character.position)
-    # print("zombie2 = ", zombie2.wander.character.position)
-    
-    zombie.draw()
-    # zombie2.draw()
-    # print("")
-    # zombie.update(3.14) #testData
-    # zombie.update([1.0, 2.2])
-    zombie.update()
-    # zombie2.update()
+
+    for zombie in zombies:
+        zombie.draw()
+        zombie.update()
+
     pygame.display.update()
